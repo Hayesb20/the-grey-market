4cb4c3bad433fa060af5e25b7e6086eeb45e447d
Brian H <hayesb20@hanovere.edu>
2023-05-16 09:56:01 -0400

    Removed Sensitive Information

--- marketplace/user.py
+++ marketplace/user.py
@@ -2,7 +2,7 @@
 # 26 Apr 2023
 
 user_info = {
-    "fb_email"  : "brianhayes1998@yahoo.com",
-    "fb_pw"     : "FirstShiftSucks2!",
+    "fb_email"  : "",
+    "fb_pw"     : "",
     "filep_to_databases" : "C:/Users/hayesb3/Geany 1.38/Program Files/marketplace/the-grey-market/marketplace/txt_files"
 }
