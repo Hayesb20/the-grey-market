1f11919f894b2f52db4bc6c3c95f23f71348ca42
Brian H <hayesb20@hanovere.edu>
2023-05-16 07:21:45 -0400

    Created a user file to hold all needed login information
    This file is referenced by any modules that need a users login information

---
+++ marketplace/user.py
@@ -0,0 +1,8 @@
+# Brian Hayes
+# 26 Apr 2023
+
+user_info = {
+    "fb_email"  : "*email address here*",
+    "fb_pw"     : "*password here*",
+    "filep_to_databases" : "C:/Users/hayesb3/Geany 1.38/Program Files/marketplace/the-grey-market/marketplace/txt_files"
+}
